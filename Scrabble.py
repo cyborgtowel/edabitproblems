96dd6ecb1ba85f9788763d82c66178e0b9d72eaf
cyborgtowel <75426920+cyborgtowel@users.noreply.github.com>
2020-12-16 19:36:07 +0800

    Update Scrabble.py

--- Scrabble.py
+++ Scrabble.py
@@ -19,29 +19,3 @@ def can_build(word, letters):
                 wildcardcount -= 1
     return result
 
-# Alternative solution
-# def can_build(word, letters):
-# 	for i in word.upper():
-# 		if i in letters:
-# 			letters.remove(i)
-# 		elif '#' in letters:
-# 			letters.remove('#')
-# 		else:
-# 			return False
-# 	return True
-
-
-print(can_build("quavers", ["S", "U", "Q", "V", "A", "#", "#"])) #, True)
-print(can_build("move", ["M", "U", "T", "V", "E", "J", "#"])) #, True)
-print(can_build("banter", ["N", "E", "B", "N", "#", "A", "T"])) #, True)
-print(can_build("snake", ["S", "K", "E", "N", "V", "J", "A"])) #, True)
-print(can_build("move", ["M", "U", "T", "V", "E", "J", "S"])) #, False)
-print(can_build("sharp", ["S", "K", "H", "#", "#", "F", "F"])) #, False)
-print(can_build("more", ["M", "R", "I", "E", "U", "S", "F"])) #, False)
-print(can_build("talker", ["#", "#", "Z", "V", "P", "A", "K"])) #, False)
-print(can_build("talker", ["#", "#", "T", "T", "A", "A", "L"])) #, False)
-print(can_build("brain", ["#", "S", "B", "B", "I", "I", "#"])) #, False)
-print(can_build("pip", ["#", "P", "I", "S", "V", "W", "Z"])) #, True)
-print(can_build("pip", ["X", "P", "I", "S", "V", "W", "Z"])) #, False)
-print(can_build("stuff", ["F", "F", "X", "S", "U", "A", "T"])) #, True)
-print(can_build("stuff", ["F", "Z", "X", "S", "U", "A", "T"])) #, False)
